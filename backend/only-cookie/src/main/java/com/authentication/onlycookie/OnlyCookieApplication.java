dfab9be7e0d3458f55aed7d6f819603f6f99a0cd
ghemulyanto <trimulyanto.tm@gmail.com>
2020-04-28 10:49:42 +0700

    first commit

---
+++ backend/only-cookie/src/main/java/com/authentication/onlycookie/OnlyCookieApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.onlycookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyCookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyCookieApplication.class, args);
+	}
+
+}
---
+++ backend/only-cookie/src/test/java/com/authentication/onlycookie/OnlyCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.onlycookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyCookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
